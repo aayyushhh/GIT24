acf5b2cbb5f594395b47f55be9c9fc13ea2dd093
harsh <kolih453@gmail.com>
2023-08-04 17:35:03 +0530

    third

--- frontend/src/components/header/navigation.jsx
+++ frontend/src/components/header/navigation.jsx
@@ -18,7 +18,7 @@ export const Navigation = (props) => {
           <a class="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#features">About Us</a>
+          <a class="nav-link" href="#features">About U</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Services</a>
